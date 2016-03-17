68c8ef558eb25398710a9a2107c8c730d6e579a9
Afforess <afforess@gmail.com>
2016-03-16 20:49:04 -0400

    Don't allow repeat trends

--- control.lua
+++ control.lua
@@ -9,10 +9,10 @@ script.on_event(defines.events.on_tick, function(event)
     game.daytime = time_of_day
 end)
 
-light_trends = {brightening = {min_length = 60 * 60 * 2, max_length = 60 * 60 * 10, odds = 50, ideal_light = 0.3, intensity =  0.4},
-                clear =       {min_length = 60 * 60 * 1, max_length = 60 * 60 * 3,  odds = 65, ideal_light = 0.75, intensity =  0.1},
-                gloomy =      {min_length = 60 * 60 * 3, max_length = 60 * 60 * 12, odds = 80, ideal_light = 0.6, intensity = -0.2},
-                darkening =   {min_length = 60 * 60 * 2, max_length = 60 * 60 * 7,  odds = 60, ideal_light = 1.0, intensity = -0.5}
+light_trends = {brightening = {min_length = 60 * 60 * 2, max_length = 60 * 60 * 10, odds = 50, intensity =  0.4},
+                clear =       {min_length = 60 * 60 * 1, max_length = 60 * 60 * 3,  odds = 65, intensity =  0.1},
+                gloomy =      {min_length = 60 * 60 * 3, max_length = 60 * 60 * 12, odds = 80, intensity = -0.2},
+                darkening =   {min_length = 60 * 60 * 2, max_length = 60 * 60 * 7,  odds = 60, intensity = -0.5}
                 }
 
 function get_available_light()
@@ -20,7 +20,7 @@ function get_available_light()
     if not global.light_trend then begin_new_light_trend("clear") end
     
     if global.trend_expires < game.tick then
-        begin_new_light_trend(choose_light_trend())
+        begin_new_light_trend(choose_light_trend(global.light_trend))
     end
 
     return math.min(1, math.max(0.14, global.current_light + global.trend_delta))
@@ -36,21 +36,16 @@ function begin_new_light_trend(trend)
     Logger.log("Beginning trend: " .. trend .. ", Expires in " .. (time_length / 60) .. "s, Delta: " .. global.trend_delta)
 end
 
-function calculate_trend_odds(trend_data)
-    local base_odds = trend_data.odds
-    local extra_odds = math.max(0, 10 - (200 * (math.abs(trend_data.ideal_light - (1 - game.darkness)))))
-    Logger.log("Trend base odds: " .. base_odds .. ", extra odds: " .. extra_odds)
-    return base_odds + extra_odds
-end
-
-function choose_light_trend()
+function choose_light_trend(excluded_trend)
     local best_roll = 0
     local best_trend = nil
     for trend, data in pairs(light_trends) do
-        local roll = math.random(calculate_trend_odds(data))
-        if roll > best_roll then
-            best_roll = roll
-            best_trend = trend
+        if trend ~= excluded_trend then
+            local roll = math.random(data.odds)
+            if roll > best_roll then
+                best_roll = roll
+                best_trend = trend
+            end
         end
     end
     
